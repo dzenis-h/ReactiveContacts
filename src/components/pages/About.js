874445b0ccc4d5adb2597f4d7acf0ab6f3440336
BiggaHD <dzenis.hankusic@gmail.com>
2018-09-16 06:33:39 +0200

    ContextAPI practice ...

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Reactive Contacts</h1>
+      <hr/>
+      <p className="lead">A simple app that helps you to manage your contacts</p>
+      <p className="text-secondary">Version 0.1.0</p>
+    </div>
+  );
+};
